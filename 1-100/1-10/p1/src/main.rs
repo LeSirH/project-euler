e6838a3ba2c9f4253ad564dd50338e3f78b33b9b
Ryan Harrington <rharrington.web@gmail.com>
2020-11-19 21:03:41 -0700

    feat: problem 1 solved

---
+++ 1-100/1-10/p1/src/main.rs
@@ -0,0 +1,19 @@
+/*
+
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    Find the sum of all the multiples of 3 or 5 below 1000.
+
+*/
+
+fn main() {
+    // I had originally attempted using i16.
+    let mut sum: i32 = 0;
+
+    for value in 0..1000 {
+        if (value % 3 == 0) || (value % 5 == 0) {
+            sum += value;
+        }
+    }
+
+    println!("{}", sum);
+}
