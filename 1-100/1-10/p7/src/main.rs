75ab3b87d8511d04e3cd344caaf1d008e8603622
Ryan Harrington <rharrington.web@gmail.com>
2020-11-21 19:06:41 -0700

    feat: problem 7 solved

---
+++ 1-100/1-10/p7/src/main.rs
@@ -0,0 +1,35 @@
+/*
+
+    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+    What is the 10 001st prime number?
+
+*/
+
+fn main() {
+    let mut prime_count: i16 = 1;
+    let mut n = 3;
+
+    while prime_count < 10001 {
+        if is_prime(n) {
+            prime_count += 1;
+        }
+
+        n += 2;
+    }
+
+    println!("{}", n - 1);
+}
+
+fn is_prime(n: i64) -> bool {
+    if n < 2 {
+        return false;
+    }
+
+    for i in 2..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    return true;
+}
