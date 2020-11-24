41610a15aaab6aea09219291716db5e2b721a0ac
Ryan Harrington <rharrington.web@gmail.com>
2020-11-23 17:21:00 -0700

    feat: problem 9 solved

--- 1-100/1-10/p9/src/main.rs
+++ 1-100/1-10/p9/src/main.rs
@@ -1,14 +1,34 @@
 /*
 
-    { Problem Instructions }
+    A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+    a2 + b2 = c2
+    For example, 32 + 42 = 9 + 16 = 25 = 52.
+    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    Find the product abc.
 
 */
 
 fn main() {
     use std::time::Instant;
     let before = Instant::now();
+    let mut solved: bool = false;
 
-    // solve
+    for a in 1..1000 {
+        if solved {
+            break;
+        }
+
+        for b in 1..1000 {
+            let c = 1000 - a - b;
+            let is_triplet = i32::pow(a, 2) + i32::pow(b, 2) == i32::pow(c, 2);
+
+            if is_triplet {
+                println!("{}", a * b * c);
+                solved = true;
+                break;
+            }
+        }
+    }
 
     println!("Elapsed time: {:.2?}", before.elapsed());
 }
