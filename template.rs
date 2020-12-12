afdf8c23eea14118f492ff90a4e32a4526ff7509
Ryan Harrington <rharrington.web@gmail.com>
2020-12-11 19:42:54 -0700

    updated template.rs

--- template.rs
+++ template.rs
@@ -8,12 +8,16 @@ use std::time::Instant;
 
 fn main() {
     display_instructions();
+
     let before = Instant::now();
+    let solution: u8 = solve();
 
-    // solve
-
-    println!("Answer: ");
+    println!("Answer: {}", solution);
     println!("Elapsed time: {:.2?}", before.elapsed());
+}
+
+fn solve() -> u8 {
+    0
 }
 
 fn display_instructions() {
